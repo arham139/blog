ee1d13bc981e88d8fbd47f7b1f5635d0344e9cbc
ALi <hammadrafiq139@gmail.com>
2023-12-29 14:07:05 +0500

    second branch

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def custom
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end  
